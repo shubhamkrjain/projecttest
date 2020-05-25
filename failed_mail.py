08817dc564a01402086979480d82414b91c45cfd
SHUBHAM KUMAR JAIN <38971147+shubhamkrjain@users.noreply.github.com>
2020-05-26 02:30:48 +0530

    final

--- failed_mail.py
+++ failed_mail.py
@@ -4,10 +4,10 @@ s = smtplib.SMTP('smtp.gmail.com', 587)
 # start TLS for security 
 s.starttls()   
 # Authentication 
-s.login("sender-mail", "password") 
+s.login("jainalokkumar5@gmail.com", "Passwrd@123") 
 # message to be sent 
 message = "Hey Developer, you need to check your code once. Might be this have some error. "
 # sending the mail 
-s.sendmail("sender-mail", "developer_mail", message) 
+s.sendmail("jainalokkumar5@gmail.com", "jainalokkumar5@gmail.com", message) 
 # terminating the session 
 s.quit()
